fb62b74d087e7f8d77f736e02d171e13104f2a3e
Lalovw <lalomotiti@gmail.com>
2017-02-14 13:32:17 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -2,5 +2,6 @@
 
 int main(){
 	printf("Hola mundo");
+	printf (me gusta que me digan lalo, pretendo ser ingeniero, y en mis ratos libres juego un poco lol, aunque soy bronce 1, pero realmente era plata 4)
 	return 0;
 }
